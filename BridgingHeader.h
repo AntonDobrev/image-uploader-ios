26ad73afdba0582bdd4dc6cad7bad0f5d66bf063
AntonDobrev <tony@95-87-214-139.net1.bg>
2015-03-16 23:03:28 +0200

    Initial commit.

---
+++ BridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  BridgingHeader.h
+//  imageshandler
+//
+//  Created by Tony on 16/03/2015.
+//  Copyright (c) 2015 dobrev. All rights reserved.
+//
+
+#ifndef imageshandler_BridgingHeader_h
+#define imageshandler_BridgingHeader_h
+
+
+#endif
+
+#import <EverliveSDK/EverliveSDK.h>
--- imageshandler/ViewController.swift
+++ imageshandler/ViewController.swift
@@ -8,11 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        println("Image Selected")
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        self.pickedImage.image = image
+        
+        let myApiKey:NSString = "YOUR_API_KEY"
+        Everlive.setApplicationKey(myApiKey)
+        
+        var imageName:String = "some image";
+        var imageData = UIImagePNGRepresentation(image)
+        
+        let file:EVFile = EVFile()
+        file.filename = imageName
+        file.data = imageData
+        file.contentType = nil
+        
+        file.save { (result:Bool, error:NSError!) -> Void in
+            if(result) {
+                println("Success")
+            } else {
+                println("Failed to create activity: " + error.domain)
+            }
+        }
+      
+        
+    }
+    
+    @IBAction func pickImageAction(sender: UIButton) {
+        var image = UIImagePickerController()
+        image.delegate = self;
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+        image.allowsEditing = false
+        
+        self.presentViewController(image, animated: true, completion: nil)
+    }
 
+    @IBOutlet var pickedImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
